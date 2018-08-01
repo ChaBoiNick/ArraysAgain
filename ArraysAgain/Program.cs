75e9f29fc0d2e18e150842457e06d6041fdcaac8
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-02 09:28:00 +1000

    Select Numbers in the array

--- ArraysAgain/Program.cs
+++ ArraysAgain/Program.cs
@@ -10,6 +10,15 @@ namespace ArraysAgain
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[5];
+            numbers[0] = 4;
+            numbers[1] = 8;
+            numbers[2] = 15;
+            numbers[3] = 16;
+            numbers[4] = 23;
+
+            Console.WriteLine(numbers[2]);
+            Console.ReadLine();
         }
     }
 }
