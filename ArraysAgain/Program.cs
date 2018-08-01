44ffcdb59af009621cae4adecb25deb673ad39d0
Nicholas Schmidt <talkschmidt@hotmail.com>
2018-08-02 09:39:43 +1000

    presentations and reverse

--- ArraysAgain/Program.cs
+++ ArraysAgain/Program.cs
@@ -10,6 +10,8 @@ namespace ArraysAgain
     {
         static void Main(string[] args)
         {
+
+            /*
             int[] numbers = new int[5];
             numbers[0] = 4;
             numbers[1] = 8;
@@ -20,6 +22,39 @@ namespace ArraysAgain
             //Console.WriteLine(numbers[2]);
             Console.WriteLine(numbers.Length); //length of the array
             Console.ReadLine();
+            */
+
+            //int[] numbers = new int[] { 4, 8, 16, 25, 90, 50 };
+
+
+            //  string[] names = new string[] { "Mark", "Eddie", "Etc" };
+            //  /*
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i]);
+            //}
+            //Console.ReadLine();
+            //*/
+
+            //  foreach (string name in names)
+            //  {
+            //      Console.WriteLine(name);
+            //  }
+            //  Console.ReadLine();
+
+            string zig = "You can get what you want our of life" + 
+                "if you help enough other people get what they want";
+
+            char[] charArray = zig.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+            Console.ReadLine();
         }
+
     }
 }
